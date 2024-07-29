c4f7d4757f184ca3c8bcabe26faf9d7a13146797
PythonerJOJO <daiyeshi666@163.com>
2024-07-30 07:08:08 +0800

    d

--- src/cpp01_topic/src/demo02_listener_str.cpp
+++ src/cpp01_topic/src/demo02_listener_str.cpp
@@ -18,15 +18,28 @@ class Listener : public rclcpp::Node
 {
 private:
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
-    void on_callback()
+    void do_callback(const std_msgs::msg::String &msg)
     {
+        // 3-2.处理订阅到的消息
+        RCLCPP_INFO(this->get_logger(), "订阅到的消息是：%s", msg.data.c_str());
     }
 
 public:
     Listener() : Node("listener_node_cpp")
     {
         RCLCPP_INFO(this->get_logger(), "订阅方创建");
-        // subscription_ = this->create_subscription<std_msgs::msg::String, this->on_callback()>("talker", 10, this->on_callback());
+        // 3-1.创建订阅方
+        /**
+         * @note    std::bind()可以将一个函数或者函数对象与其部分参数绑定在一起,生成一个新的可调用对象
+         * 模板：消息类型
+         * @param
+         *      1.话题名
+         *      2.QOS,队列长度
+         *      3.回调函数，回调函数中要处理订阅到的数据(要绑定的成员函数指针,要绑定的对象指针:也就是当前 Listener 类的实例,哪一个参数会被传递给 do_callback 函数)
+         * @return  订阅对象指针
+         */
+        subscription_ = this->create_subscription<std_msgs::msg::String>("chatter", 10,
+                                                                         std::bind(&Listener::do_callback, this, std::placeholders::_1));
     }
 };
 
--- src/py01_topic/py01_topic/demo01_talker_str_py.py
+++ src/py01_topic/py01_topic/demo01_talker_str_py.py
@@ -1,6 +1,47 @@
-def main():
-    print('Hi from py01_topic.')
+"""  
+    需求：以某个固定频率发送文本“hello world!”，文本后缀编号，每发送一条消息，编号递增1。
+    步骤：
+        1.导包；
+        2.初始化 ROS2 客户端；
+        3.定义节点类；
+            3-1.创建发布方；
+            3-2.创建定时器；
+            3-3.组织消息并发布。
+        4.调用spin函数，并传入节点对象；
+        5.释放资源。
+"""
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
 
+class Talker(Node):
+    def __init__(self):
+        super.__init__("talker_node_py")
+        self.get_logger().info("发布方创建(python)!")
+        # 3-1.创建发布方
+        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        # 3-2.创建定时器
+        self.timer = self.create_timer(1.0, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = 'Hello World(py):%d'%self.i
+        self.publisher_.publish(msg)
+        self.get_logger().info('发布的消息："%s"'% msg.data)
+        self.i +=1
+
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    rclpy.spin(Talker)  # 调用spin函数，并传入节点对象
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
+
+
+
+
